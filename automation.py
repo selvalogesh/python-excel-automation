7ce9f51c151b4dd6ba141097c28c3196285be855
Selva Logesh <selvalogesh95@gmail.com>
2020-08-25 18:25:22 +0530

    Condition added to download multiple images

--- automation.py
+++ automation.py
@@ -3,31 +3,34 @@ import requests
 import pandas as pd
 from PIL import Image
 
-dfs = pd.read_excel("X STD MATHS.xlsx", sheet_name="SheetJS")
+dfs = pd.read_excel("PMT DES 8 A & B.xlsx", sheet_name="SheetJS")
 s = requests.Session()
 
 for row in range(len(dfs)-1):
     imagelist = []
     firstImg = None
     name = dfs["Student Name"][row]
-    roll = int(dfs["Roll No./Register No"][row])
+    roll = dfs["Roll No./Register No"][row]
 
     for column in dfs:
         if (column.find("-") != -1):
             try:
-                url = dfs[column][row]
-                #print(url)
-                response = s.get(url.split()[0], stream=True)
-                #print(response.status_code)
-                if(response.status_code == 200):
-                    img = Image.open(response.raw)
-                    if(firstImg == None):
-                        firstImg = img
+                urlRow = dfs[column][row]
+                urls = [x.strip() for x in urlRow.split()]
+                for url in urls:
+                    response = s.get(url, stream=True)
+                    #print(response.status_code)
+                    if(response.status_code == 200):
+                        img = Image.open(response.raw)
+                        if(img.mode == 'RGBA'):
+                            img = img.convert('RGB')
+                        if(firstImg == None):
+                            firstImg = img
+                        else:
+                            imagelist.append(img)
                     else:
-                        imagelist.append(img)
-                else:
-                    NoneImg = Image.open("None.jpg")
-                    imagelist.append(NoneImg)
+                        NoneImg = Image.open("None.jpg")
+                        imagelist.append(NoneImg)
             except:
                 with open("./pdf/error.txt",'at') as err:
                     err.write("{0}_{1}_{2} in {3} -> {4}\n".format(row, roll, name, column, dfs[column][row]))
